bd7f1729bc7e41f058fdb428eb658630eee45dd0
sejoung kim <sejoung@gmail.com>
2024-09-02 17:13:40 +0900

    unet 추가

--- services/comfy/entrypoint.sh
+++ services/comfy/entrypoint.sh
@@ -11,7 +11,6 @@ MOUNTS["${ROOT}/input"]="/data/config/comfy/input"
 MOUNTS["${ROOT}/output"]="/output/comfy"
 MOUNTS["${ROOT}/custom_nodes"]="/data/config/comfy/custom_nodes"
 MOUNTS["${ROOT}/models/xlabs/ipadapters"]="/data/models/ipadapter/flux"
-MOUNTS["${ROOT}/models/xlabs/flux"]="/data/models/Stable-diffusion//flux"
 
 
 for to_path in "${!MOUNTS[@]}"; do
